b743eafb0f02ad7be5247c93871622fbd6f4a71b
i_inrita <111368957+inrita@users.noreply.github.com>
2023-10-30 22:24:01 +0700

    Update bookStore.js

--- INT201-main/Module2/BookStore/bookStore.js
+++ INT201-main/Module2/BookStore/bookStore.js
@@ -7,28 +7,26 @@ class BookStore {
         this.stores=[]
     }
     findBookIndex(isbn){
-        return this.stores.findIndex(a=>a.isbn===isbn);
+        return this.stores.findIndex(a => a.isbn === isbn);
     }
     addNewBook(isbn, stocks){
-        if(this.findBookIndex(isbn)===-1){
-            this.stores.push({isbn: isbn, stocks: stocks});
+        if(this.findBookIndex(isbn)===-1){ //ไปดู isbn จาก findBookIndex(isbn) ถ้าไม่มี isbn เก็บอยู่ใน stores 
+            this.stores.push({isbn: isbn, stocks: stocks}); //ให้สร้าง two property add เข้าไป
             return stocks
         } else return -1
     }
     sellBook(isbn, quantity){
-        if(this.findBookIndex(isbn)===-1){
+        if(this.findBookIndex(isbn)===-1){ //ต้องการเข้าถึงใน array
             return -1
         } else{
             let find = this.stores[this.findBookIndex(isbn)]
             if(find.stocks<quantity){
                 return 0
-            } else find.stocks -= quantity
+            } else find.stocks -= quantity // quantity - stocks
+            // find.stocks = find.stocks - quantity
             return quantity
         }
     }
-
-
-
 }
 
 module.exports = BookStore
