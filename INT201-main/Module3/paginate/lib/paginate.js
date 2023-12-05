f354a661246f704322b6dbef95db29222f4ff790
i_inrita <111368957+inrita@users.noreply.github.com>
2023-12-05 20:00:18 +0700

    Update paginate.js

--- INT201-main/Module3/paginate/lib/paginate.js
+++ INT201-main/Module3/paginate/lib/paginate.js
@@ -1,3 +1,4 @@
+// getItemsOfCurrentPage(allItems, currentPage, rowsPerPage) returns an array of items from allItems that calculates on a given current page and rows per page.
 function getItemsOfCurrentPage(allItems, currentPage, rowsPerPage) {
   if (allItems === null || allItems === undefined) return undefined
   if (allItems.length === 0) return []
@@ -7,9 +8,10 @@ function getItemsOfCurrentPage(allItems, currentPage, rowsPerPage) {
     rowsPerPage * (currentPage - 1),
     rowsPerPage * (currentPage - 1) + rowsPerPage
   )
-
   return itemsOfCurrentPage.length > 0 ? itemsOfCurrentPage : []
 }
+
+// getTotalPages(allItems, rowsPerPage) returns the total number of pages by calculating from a given allItems’s length and rows per page
 function getTotalPages(allItems, rowsPerPage) {
   if (allItems === null || allItems === undefined) return undefined
   if (rowsPerPage === 0 || rowsPerPage === undefined || rowsPerPage === null)
